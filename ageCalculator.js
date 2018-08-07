2efcc347df23307ec60b64e67795d9aadd0b69e4
Dave <dave.samson91@gmail.com>
2018-08-07 18:57:28 +0000

    JS basics, arrays, if statements

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return name + ' is ' + (currentYear - yearOfBirth) + ' years old.'
+}
+console.log(ageCalculator("Suzie", 1986, 2018));
+console.log(ageCalculator("Jack", 2006, 2018));
+console.log(ageCalculator("Ali", 2018, 2018));
---
+++ arrayBasics.js
@@ -0,0 +1,4 @@
+var director = 'Steven Spielberg';
+var films = ['Jaws', 'Raiders of the Lost Ark', 'A.I'];
+console.log(director)
+console.log(films)
